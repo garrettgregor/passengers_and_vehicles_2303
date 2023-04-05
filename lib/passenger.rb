5c62028f97f7ba1f50c58bda09ea1a0d0b938283
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-05 09:09:34 -0600

    Add passenger with name and age attributes

---
+++ lib/passenger.rb
@@ -0,0 +1,9 @@
+class Passenger
+  attr_reader :name,
+              :age
+
+  def initialize(passenger_details)
+    @name = passenger_details["name"]
+    @age = passenger_details["age"]
+  end
+end
