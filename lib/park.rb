31f675d8945b2f306c6a760444f658af4aa0c274
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-05 10:36:18 -0600

    Verify that park can get list of passengers from multiple vehicles

--- lib/park.rb
+++ lib/park.rb
@@ -2,7 +2,8 @@ class Park
   attr_reader :name,
               :admission_price,
               :vehicles,
-              :passengers
+              :passengers,
+              :revenue
   def initialize(name, admission_price)
     @name             = name
     @admission_price  = admission_price
