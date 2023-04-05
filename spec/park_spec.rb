a9b4b73fd4c7a6691c202664201ffaeb85493c79
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-05 10:03:32 -0600

    Add park and establish that it is initialized with a name and admission price

---
+++ spec/park_spec.rb
@@ -0,0 +1,16 @@
+require "./lib/passenger"
+require "./lib/vehicle"
+require "./lib/park"
+require "rspec"
+
+RSpec.describe Park do
+  describe "initialize" do
+    it "exists and has attributes" do
+      yosemite = Park.new("Yosemite", 50)
+
+      expect(yosemite).to be_a(Park)
+      expect(yosemite.name).to eq("Yosemite")
+      expect(yosemite.admission_price).to eq(50)
+    end
+  end
+end
