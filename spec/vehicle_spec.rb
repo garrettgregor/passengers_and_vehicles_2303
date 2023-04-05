0355a56283b319fee75a4a2858ee26c1a4285d19
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-05 09:51:08 -0600

    Allows vehicle to add passengers

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe Passenger do
       expect(vehicle.make).to eq("Honda")
       expect(vehicle.model).to eq("Civic")
       expect(vehicle.speeding).to be false
+      expect(vehicle.passengers).to eq([])
     end
   end
   
@@ -35,11 +36,26 @@ RSpec.describe Passenger do
     end
   end
   
-  describe "passenger" do
-    it "checks the number of passengers in the vehicle" do
+  describe "passengers" do
+    it "creturns the passengers in the vehicle" do
       vehicle = Vehicle.new("2001", "Honda", "Civic")
 
       expect(vehicle.passengers).to eq([])
+    end
+  end
+  
+  describe "add_passenger" do
+    it "can add passengers to the vehicle" do
+      vehicle = Vehicle.new("2001", "Honda", "Civic")
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})    
+      jude = Passenger.new({"name" => "Jude", "age" => 20})    
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})    
+      
+      vehicle.add_passenger(charlie)    
+      vehicle.add_passenger(jude)    
+      vehicle.add_passenger(taylor)    
+
+      expect(vehicle.passengers).to eq([charlie, jude, taylor])
     end
   end
 end
